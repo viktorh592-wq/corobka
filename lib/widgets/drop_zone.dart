34e25c83249b33a24fb7cbca1691b01dd6b2295d
viktorh592-wq <viktorh592@gmail.com>
2026-08-18 12:18:43 +0300

    Update drop_zone.dart

--- lib/widgets/drop_zone.dart
+++ lib/widgets/drop_zone.dart
@@ -1,3 +1,4 @@
+import 'package:cross_file/cross_file.dart';
 import 'package:desktop_drop/desktop_drop.dart';
 import 'package:flutter/material.dart';
 
@@ -29,8 +30,8 @@ class DropZone extends StatelessWidget {
     );
   }
 
-  Future<void> _handleDragDone(List<DropItem> files) async {
-    final paths = files.map((f) => f.path).whereType<String>().toList();
+  Future<void> _handleDragDone(List<XFile> files) async {
+    final paths = files.map((f) => f.path).toList();
     await controller.importPaths(paths);
     onImported?.call();
   }
